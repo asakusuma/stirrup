274b0bce3552f73c3c72b9f43b14611e67bd46c5
asakusuma <asa.kusuma@gmail.com>
2014-05-19 20:02:42 -0700

    Add RSVP

---
+++ config/rsvp.js
@@ -0,0 +1,14 @@
+module.exports = {
+  constructor: 'Promise',
+  defer: 'defer',
+  staticFuncs: [
+    {
+      libName: 'all',
+      nativeName: 'all',
+      aliases: [
+        'all',
+        'when'
+      ]
+    }
+  ]
+};
